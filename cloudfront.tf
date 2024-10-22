3c746c495cea111da8fed9840dd7d5f381340e28
dinesh-radhakrishnan <33116415+dinesh127@users.noreply.github.com>
2024-10-21 22:57:55 -0400

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -4,9 +4,9 @@ resource "aws_cloudfront_distribution" "app_distribution" {
     origin_id   = "app-alb"
     custom_origin_config {
       http_port              = 80
-      https_port             = 443  # Required even if not used
-      origin_protocol_policy = "http-only"  # Set to HTTP only
-      origin_ssl_protocols   = ["TLSv1.2"]  # Still required, let's keep it
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
@@ -16,12 +16,12 @@ resource "aws_cloudfront_distribution" "app_distribution" {
 
   default_cache_behavior {
     target_origin_id       = "app-alb"
-    viewer_protocol_policy = "allow-all"  # Set to HTTP and HTTPS
-    allowed_methods        = ["GET", "HEAD"]  # Adjusted allowed methods
+    viewer_protocol_policy = "allow-all"
+    allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
 
-    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"  # Amazon's caching-optimized cache policy ID
-    origin_request_policy_id = "6885a789-2217-4ade-b2e8-97286b08e9ef"  # Amazon's origin request policy ID (optional)
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    origin_request_policy_id = "6885a789-2217-4ade-b2e8-97286b08e9ef"
 
     min_ttl     = 0
     default_ttl = 3600
@@ -34,5 +34,10 @@ resource "aws_cloudfront_distribution" "app_distribution" {
     geo_restriction {
       restriction_type = "none"
     }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    ssl_support_method             = "sni-only"
   }
 }
